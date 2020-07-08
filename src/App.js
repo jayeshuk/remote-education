442c110ad865e964f1ca812f04205f8294c8145b
jayeshuk <jayeshukalkar@gmail.com>
2020-07-08 23:12:40 +0530

    Server

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import Carousel from "./Components/HomePage/Carous";
 import Footer from "./Components/HomePage/Footer";
 import Jumbo from "./Components/HomePage/Jumbo";
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log(
-    "Express server listening on port %d in %s mode",
-    this.address().port,
-    app.settings.env
-  );
-});
+// app.listen(process.env.PORT || 3000, function () {
+//   console.log(
+//     "Express server listening on port %d in %s mode",
+//     this.address().port,
+//     app.settings.env
+//   );
+// });
 
 function App() {
   return (
