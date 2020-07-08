74eb348f61475bfe932c2a9238cd32cd03fda585
jayeshuk <jayeshukalkar@gmail.com>
2020-07-08 23:00:13 +0530

    PORT

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import Carousel from "./Components/HomePage/Carous";
 import Footer from "./Components/HomePage/Footer";
 import Jumbo from "./Components/HomePage/Jumbo";
 
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
+});
+
 function App() {
   return (
     <div className="App">
